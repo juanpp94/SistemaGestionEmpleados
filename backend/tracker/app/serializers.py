cc7ba285520709a3968d36960d0d6d9ced088254
Kevin Chacón <13-10268@usb.ve>
2020-03-11 13:34:35 -0400

    Setup inicial del proyecto en django

--- backend/tracker/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-from .models import Usuario
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ( 'email', 'password', 'position')
