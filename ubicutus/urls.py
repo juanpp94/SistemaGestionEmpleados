35d046a55cb7d7df224b5e02b0949a9e63a68b63
LCMARVAL <luiscarm77@gmail.com>
2020-03-13 03:38:44 -0400

    Templates agarrando estilo

--- ubicutus/urls.py
+++ ubicutus/urls.py
@@ -5,8 +5,10 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.ingreso_usuario,name='ingreso'),
+    url(r'^registro',views.registro,name='registro'),
     url(r'^index1/',views.index1,name='index1'),
+    url(r'^index/',views.index,name='index'),
     url(r'^app/',include('app.urls')),
     url(r'^salir/$', views.salir, name='salir'),
     url(r'^usuario/',include('apps.perfil.urls')),
