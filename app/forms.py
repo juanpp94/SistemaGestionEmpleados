d566902f1d45abfb908123b9badd90026e811130
LCMARVAL <luiscarm77@gmail.com>
2020-03-19 23:59:54 -0400

    Formulario Usuario Registro

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,9 @@ from app.models import InformacionUsuario
 from django.contrib.auth.models import User
 
 class Formulario_Usuario(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.EmailField()
+    username = forms.CharField(widget=forms.TextInput(attrs = { 'class': 'form-control'} ))
+    password = forms.CharField(widget=forms.PasswordInput(attrs = { 'class': 'form-control'} ))
+    email = forms.EmailField(widget=forms.EmailInput(attrs = { 'class': 'form-control'} ))
     class Meta():
         model = User
         fields = ('username','password','email')
