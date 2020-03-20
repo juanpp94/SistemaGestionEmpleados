c25585a55ace51c8eb8e52a61b87434a41c729f9
LCMARVAL <luiscarm77@gmail.com>
2020-03-19 22:50:50 -0400

    Auth Styles and email validation

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import User
 
 class Formulario_Usuario(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField()
     class Meta():
         model = User
         fields = ('username','password','email')
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith('@ubicutus.com'):
+            raise forms.ValidationError("No es un correo @ubicutus.com")
+        return email
 
 class Formulario_Datos_Perfil(forms.ModelForm):
 	class Meta():
