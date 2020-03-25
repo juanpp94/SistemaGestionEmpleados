d7a6335dceaff3b4f569719efaccb8dd5a1aab2e
Benitez4721 <benitez4721@gmail.com>
2020-03-24 23:35:41 -0400

    limpiar

--- ubicutus/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class InformacionUsuario(models.Model):
-	user = models.OneToOneField(User,on_delete=models.CASCADE)
-	nombre = models.CharField(max_length=40, blank = True)
-	apellido = models.CharField(max_length=40, blank = True)
-	foto_de_perfil = models.ImageField(upload_to='foto_de_perfil',blank=True)
-	
