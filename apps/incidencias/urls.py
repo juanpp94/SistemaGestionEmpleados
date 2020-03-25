21132f05803640892c01ceb42d43e43f3177d966
LCMARVAL <luiscarm77@gmail.com>
2020-03-25 12:24:34 -0400

    Descartar Incidencia

--- apps/incidencias/urls.py
+++ apps/incidencias/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import incidencias_list_view, incidencia_crear, incidencia_mostrar, incidencia_editar
+from .views import incidencias_list_view, incidencia_crear, incidencia_mostrar, incidencia_editar, incidencia_descartar
 
 app_name = 'incidencias'
 urlpatterns=[
@@ -7,4 +7,5 @@ urlpatterns=[
     url(r'^reportar',incidencia_crear, name = 'reportar'),
     url(r'^(?P<pk>\d+)$',incidencia_mostrar, name = 'mostrar'),
     url(r'^editar/(?P<pk>\d+)$',incidencia_editar, name = 'editar'),
+    url(r'^eliminar/(?P<pk>\d+)$',incidencia_descartar, name = 'eliminar'),
 ]
--- apps/incidencias/views.py
+++ apps/incidencias/views.py
@@ -9,8 +9,9 @@ def incidencias_list_view(request):
     user= request.user.id
     perfil = InformacionUsuario.objects.get(user_id=user)
     queryset = Incidencia.objects.filter(usuario_id=user)
+    incidencias_activas = queryset.exclude(estatus="Descartar")
     context = {
-        "incidencias_list": queryset,
+        "incidencias_list": incidencias_activas,
         "perfil": perfil,
     }
     return render(request, "incidencias_list.html", context)
@@ -30,6 +31,7 @@ def incidencia_crear(request):
             from_email = settings.EMAIL_HOST_USER
             addresses = [user_email, settings.EMAIL_HOST_USER]
             send_mail(subject, message, from_email, addresses, fail_silently=True)
+            return redirect('incidencias:mostrar', user)
     else:
         form = IncidenciaForm()
     return render(request,'incidencias_form.html',{'form':form, 'perfil':perfil})
@@ -53,8 +55,30 @@ def incidencia_editar(request, pk):
             incidencia = form.save(commit=False)
             incidencia.usuario_id = user
             incidencia.save()
+            return redirect('incidencias:mostrar', user)
     context = {
         'form':form, 
         'perfil':perfil
     }
     return render(request,'incidencias_form.html',context)
+
+def incidencia_descartar(request, pk):
+    user= request.user.id
+    perfil = InformacionUsuario.objects.get(user_id=user)
+    incidencia = Incidencia.objects.get(id=pk)
+    form = IncidenciaForm(data=request.POST, instance=incidencia)
+    if request.method == 'GET':
+        form = IncidenciaForm(instance=incidencia)
+    else:
+        if form.is_valid():
+            des_incidencia = form.save(commit=False)
+            des_incidencia.estatus = "Descartar"
+            incidencia.usuario_id = user
+            des_incidencia.save()
+            return redirect('incidencias:lista')
+    context = {
+        'form':form,
+        'incidencia':incidencia,
+        'perfil':perfil,
+    }
+    return render(request, 'incidencia_eliminar.html', context)
