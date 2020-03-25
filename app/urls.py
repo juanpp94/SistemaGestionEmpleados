9aa66b3e1acb055123380dbcfcd1de5a6dd274b6
Benitez4721 <benitez4721@gmail.com>
2020-03-25 13:36:18 -0400

    limpiar

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from app import views
-app_name = 'app'
-urlpatterns=[
-    url(r'^registro/$',views.registro,name='registro'),
-    url(r'^ingreso/$',views.ingreso_usuario,name='ingreso'),
-    url(r'^index/$',views.index,name='index'),
-]
--- apps/incidencias/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IncidenciasConfig(AppConfig):
-    name = 'incidencias'
