089f6f21306f0ea7162ddc1627c2b05c81213d3e
VTorres8 <12-11468@usb.ve>
2020-03-25 16:13:58 -0400

    Integrado el cronometro de Albert a la vista de tareas donde se cuenta el tiempo

--- ubicutus/settings.py
+++ ubicutus/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ubic',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'zack7zell8',
         'HOST': 'localhost',
         'PORT': 5432,
     }
