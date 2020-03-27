4c7fea908294039b756e22e01f4c05bcb8d7289a
LCMARVAL <luiscarm77@gmail.com>
2020-03-27 11:38:06 -0400

    Tarea Admin

--- apps/tareas/admin.py
+++ apps/tareas/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from apps.tareas.models import Tarea
 
-admin.site.register(Tarea)
+class TareaAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('usuario', 'nombre_tarea', 'status')
+
+admin.site.register(Tarea, TareaAdmin)
